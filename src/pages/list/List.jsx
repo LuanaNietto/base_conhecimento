dfa5808dc265abafab1670bd68c8e7a0b55b5d24
Gabriel Michels <gabrielformigonimichels@gmail.com>
2023-12-08 00:05:16 -0300

    links na lista de usuarios e artigo

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { AuthContainer, Section } from "./ListStyled";
-import { getAllPosts} from "../../services/articleService";
+import { getAllPosts } from "../../services/articleService";
 import { getAllUsers } from "../../services/userService";
+import { getArtigoById } from "../../services/articleService";
+import { getUserById } from "../../services/userService";
 
 export function List() {
-
   const [artigos, setArtigos] = useState([]);
   const [usuarios, setUsuarios] = useState([]);
-  
+
   async function findPost() {
     const postsResponse = await getAllPosts();
     setArtigos(postsResponse.data);
@@ -25,22 +27,26 @@ export function List() {
 
   return (
     <AuthContainer>
-    <Section type="artigo">
-    <h2 style={{ marginBottom: '10px' }}>Artigos</h2>
-      <ul>
-        {artigos.map(artigo => (
-          <li>{artigo.kb_title}</li>
-        ))}
-      </ul>
-    </Section>
-    <Section type="usuario">
-      <h2>Usuários</h2>
-      <ul>
-        {usuarios.map(usuario => (
-          <li>{usuario.author_name}</li>
-        ))}
-      </ul>
-    </Section>
+      <Section type="artigo">
+        <h2>Artigos</h2>
+        <ul>
+          {artigos.map((artigo) => (
+            <li key={artigo.id}>
+              <Link to={'/teste/'}>{artigo.kb_title}</Link>
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <Section type="usuario">
+        <h2>Usuários</h2>
+        <ul>
+          {usuarios.map((usuario) => (
+            <li key={usuario.id}>
+              <Link to={`/teste/}`}>{usuario.author_name}</Link>
+            </li>
+          ))}
+        </ul>
+      </Section>
     </AuthContainer>
   );
-};
+}
